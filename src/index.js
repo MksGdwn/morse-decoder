ae63b2e4389ca3876e8bc6864d041bcc67ce1d9e
Mksgdwn <mksgdwn@gmail.com>
2023-08-07 18:47:43 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = addZeros(expr);
+
+    const arr = [];
+
+    while (expr.length > 0) {
+        arr.push(expr.slice(0, 10));
+        expr = expr.slice(10);
+    }
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr[i];
+        let symbol = "";
+
+        while (str.length > 0) {
+            let sign = str.slice(0, 2);
+
+            if(sign === "10") {
+                symbol += ".";
+            } else if (sign === "11") {
+                symbol += "-";
+            }
+
+            str = str.slice(2);
+        }
+
+        result.push(symbol);
+    }
+
+    return result.map(el => {
+        if(!el) {
+            return " ";
+        }
+
+        return MORSE_TABLE[el];
+    }).join("");
+}
+
+function addZeros(expr) {
+    if (expr.length % 10) {
+        let count = expr.length % 10;
+
+        for (let i = 0; i < count; i++) {
+            expr = 0 + expr;
+        }
+
+        return expr;
+    } else {
+        return expr;
+    }
 }
 
 module.exports = {
